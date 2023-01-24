f8fb9767ddf81598704d008cd6b3f89c070d297f
moreno-m2698 <moreno.mw2698@gmail.com>
2023-01-24 10:08:26 -0600

    Added the ui element for the login

--- botsandbox.py
+++ botsandbox.py
@@ -23,31 +23,66 @@ async def on_ready():
     await starting_channel.send("SPICY TUNA - STATUS: ONLINE - IN TESTING")
 
 #This creates the rpg account
+class ClassSelect(discord.ui.Select):
+    def __init__(self):
+        super().__init__(placeholder="Choose a class!", value = ["Warrior", "Paladin", "Mage", "Undead"])
+    
+    async def callback(self, interaction):
+        await interaction.response.edit_message(content = f"Your chosen class is: {self.value}")
+
 
 @bot.command()
-async def start(called_channel):
-    account = True
-    if account: # This checks to see if the discord profile already has an account tied to it
-        test_modal = discord.ui.Modal(title = "This is a modal test", timeout = 180)
-        password1 = discord.ui.TextInput(label = "TextInput test")
-        test_modal.add_item(password1)
+async def login(context_channel): #This command creates a new session as well as gives the opportunity to make an account
+    yes_button = discord.ui.Button(label = 'Yes', style = discord.ButtonStyle.green)
+    async def yes_button_callback(interaction):
+        embed = discord.Embed(title = "You're logged in!", description="If this is your first time logging-in use the !class command to choose your class.")
+        await interaction.response.edit_message(content ="You have an account yay!", embed = embed, view = None)
+        #send the embed for the next part
+    yes_button.callback = yes_button_callback
 
-    # This one is to verify that the user typed in the correct password
+    no_button = discord.ui.Button(label = 'No', style = discord.ButtonStyle.red)
+    async def no_button_callback(interaction):
         
-    # Think about if we want a seperate embed object, a quickly generated one, or one stored in the view
-        await called_channel.send(test_modal)
+        await interaction.response.send_modal(AccountModal())
+    
+    no_button.callback = no_button_callback
+    view = discord.ui.View()
 
-        # After creating password we can then go into the hero choice part of the program
+    async def interaction_check(interaction):
+        if interaction.user != context_channel.author:
+            await interaction.response.send_message(content = "This is not your instance!", ephemeral=True)
+            return False
+        else:
+            return True
+    view.interaction_check=interaction_check
+
+    view.add_item(yes_button)
+    view.add_item(no_button)
+    await context_channel.send(embed=discord.Embed(title="Let's begin your journey", description="Already have an account?"),  view = view)
 
 
 
-    # implement class system 
 
+class AccountModal(discord.ui.Modal):
+    def __init__(self, *, title: str = "Create a Account") -> None:
+        super().__init__(title=title)
+
+        self.add_item(discord.ui.TextInput(label = "Username"))
+        self.add_item(discord.ui.TextInput(label = "Password", min_length=8, max_length=16,placeholder="Password must be between 8 to 16 characters"))
+
+    async def on_submit(self, interaction: discord.Interaction):
+        embed = discord.Embed(title = "You're logged in!", description="If this is your first time logging-in use the !class command to choose your class.")
+        embed.add_field(name="Username", value  = self.children[0].value)
+        embed.add_field(name="Password", value = self.children[1].value)
+        embed.add_field(name= "userid", value = interaction.user.id)
+        # write if statements that see if the username is already in the database and return on_error
+        await interaction.response.edit_message(embed=embed, view = None)
+    
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class MyModal(discord.ui.Modal):
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs, title  = "Modal via Bittpm")
+        super().__init__(*args, **kwargs, title  = "Modal via Buttom")
 
         self.add_item(discord.ui.TextInput(label="Short Input"))
         self.add_item(discord.ui.TextInput(label="Long Input", style=discord.TextStyle.long))
@@ -60,15 +95,35 @@ class MyModal(discord.ui.Modal):
         await interaction.response.send_message(embeds=[embed])
 
 class MyView(discord.ui.View):
-    @discord.ui.button(label = "Send Modal")
+    @discord.ui.button(label = "Login")
     async def button_callback(self, interaction, button):
-        await interaction.response.send_modal(MyModal())
+        await interaction.response.send_modal(LoginModal())
 
-@bot.command()
-async def modal(ctx):
-    await ctx.send(view=MyView())
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Class_Select_Embed(discord.Embed):
+    def __init__(self):
+        super().__init__(title='Choose your class!')
+        self.add_field(name="Warrior", value = 'Warrior',inline=False)
+        self.add_field(name="Paladin", value = "Paladin",inline=False)
+        self.add_field(name='Mage', value="Mage", inline =False)
+        self.add_field(name = "Undead", value = "Undead", inline=False)
+
+
+@bot.command()
+async def classes(context_channel):
+    class_select = discord.ui.Select(placeholder="Choose a class!", min_values=1, max_values=1, options=[discord.SelectOption(label = "Warrior", value = "Warrior"), discord.SelectOption(label = "Paladin", value = "Paladin")])
+    view = discord.ui.View()
+    view.add_item(class_select)
+    await context_channel.send(embed = Class_Select_Embed(), view = view)
+    
+
+
+
+
+
+
 
 @bot.command()
 async def testing(called_channel):
@@ -120,25 +175,7 @@ async def rpg(called_channel):
 
 
 
-@bot.command()
-async def login(called_channel):
-    # This function is going to mostly be a flex to make the db feel like a traditional login experience 
-    #   We can legacy this later if we want since realistically we can tie the account information to the discord account instead and use that to transfer to the cloud
 
-    # We can also maybe just have an account password so that u can itll check to see if uve done its safe
-    # see if we can create a private view
-    login_embed = discord.Embed(title = 'Login', description = 'Please give us your password')
-
-    # use a modal
-
-    password = discord.ui.TextInput()
-
-    # Do some logic to see if the user actually got the correct password
-    # Include account recovery step here
-    # Change state of the account to a logged-in state
-    # See if we can create an afk timer where if there is no activity in 90 mins itll auto-logout
-
-    pass
 
 
 
